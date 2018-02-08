3f37e687f6f490eb466723ebddbd6fbe10fd95ad
gsfbuildbot <support@gridprotectionalliance.org>
2018-02-08 00:29:40 -0500

    PDQTracker: Version change for build v1.2.177.0-master.

--- Source/Applications/PDQTracker/PDQTracker/Properties/AssemblyInfo.cs
+++ Source/Applications/PDQTracker/PDQTracker/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.2.176.0")]
+[assembly: AssemblyVersion("1.2.177.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
